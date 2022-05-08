b1c3bd2a93f9eaa841bedb21ad6a8db667cc109d
cwiklam <cwikla.m0@gmail.com>
2022-05-08 15:06:33 +0200

    Added navbar, home and products page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#landing_page'
   resources :products, only: %i[index]
   # Defines the root path route ("/")
   # root "articles#index"
