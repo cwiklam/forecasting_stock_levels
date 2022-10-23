cde47978e22e936afe09bcb71c5196874fd8406c
cwiklam <cwikla.m0@gmail.com>
2022-10-23 11:01:23 +0200

    Added min max on chart, todo: count of min i max value pass to chart controller

--- app/javascript/controllers/forecastingcharts_controller.js
+++ app/javascript/controllers/forecastingcharts_controller.js
@@ -7,6 +7,10 @@ export default class extends Controller {
     connect() {
         this.count = this.optionTarget.dataset.count
         this.max = this.optionTarget.dataset.max
+        this.yMin = parseInt(this.optionTarget.dataset.ymin) > 0 ? 0 : parseInt(this.optionTarget.dataset.ymin)
+        this.yMax = ((parseInt(this.max) + 1000) / 1000 ) * 1000
+        console.log(this.yMin)
+        console.log(this.yMax)
         this.render();
     }
 
@@ -14,21 +18,21 @@ export default class extends Controller {
         if (!this.ele) return;
 
         const ctx = this.ele.getContext('2d');
-        console.log(parseFloat(this.count))
-        const color = (parseFloat(this.count) < 0 )? 'rgb(240, 150, 150, 0.4)' : 'rgb(60, 200, 240, 0.4)'
-        console.log(String(parseInt(String(this.max * 0.6))))
+        const color = (parseFloat(this.count) < 0) ? 'rgb(240, 150, 150, 0.4)' : 'rgb(60, 200, 240, 0.4)'
         this.config = {
             data: {
                 datasets: [{
                     type: 'bar',
                     label: 'Zasoby',
                     data: [0, this.count, 0],
+                    yAxisID: 'y',
                     borderColor: 'rgba(60, 200, 240, 0.4)',
                     backgroundColor: color
                 }, {
                     type: 'line',
                     label: 'Maksymalny stan',
                     data: [this.max, this.max, this.max],
+                    yAxisID: 'y',
                     borderColor: 'rgba(50, 255, 50, 0.7)',
                     pointBackgroundColor: 'rgba(50, 255, 50, 0)',
                     pointBorderColor: 'rgba(50, 255, 50, 0)'
@@ -36,6 +40,7 @@ export default class extends Controller {
                     type: 'line',
                     label: 'Stan średni',
                     data: [parseInt(String(this.max * 0.6)), parseInt(String(this.max * 0.6)), parseInt(String(this.max * 0.6))],
+                    yAxisID: 'y',
                     borderColor: 'rgba(255, 255, 50, 0.7)',
                     pointBackgroundColor: 'rgba(255, 255, 50, 0)',
                     pointBorderColor: 'rgba(255, 255, 50, 0)'
@@ -43,6 +48,7 @@ export default class extends Controller {
                     type: 'line',
                     label: 'Stan niski',
                     data: [parseInt(String(this.max * 0.3)), parseInt(String(this.max * 0.3)), parseInt(String(this.max * 0.3))],
+                    yAxisID: 'y',
                     borderColor: 'rgba(255, 50, 50, 0.7)',
                     pointBackgroundColor: 'rgba(255, 50, 50, 0)',
                     pointBorderColor: 'rgba(255, 50, 50, 0)'
@@ -50,6 +56,7 @@ export default class extends Controller {
                     type: 'line',
                     label: 'Stan niski',
                     data: [0, 0, 0],
+                    yAxisID: 'y',
                     borderColor: 'rgba(255, 255, 255, 0.7)',
                     pointBackgroundColor: 'rgba(255, 255, 255, 0)',
                     pointBorderColor: 'rgba(255, 255, 255, 0)'
@@ -67,7 +74,17 @@ export default class extends Controller {
     }
 
     get options() {
-        const defaultOptions = {responsive: true, maintainAspectRatio: false};
+        const defaultOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    id: 'y',
+                    min: this.yMin,
+                    max: this.yMax
+                }
+            }
+        };
         return defaultOptions;
     }
 }
